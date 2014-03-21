a3996ba3feb00639aee4b62ab9052e040c13fa97
Adam Bergkvist <adam.bergkvist@ericsson.com>
2014-03-21 13:46:53 +0100

    Archived version 20140321.

--- getusermedia.js
+++ getusermedia.js
@@ -19,7 +19,7 @@ var respecConfig = {
    // copyrightStart: "2005",
 
    // if there is a previously published draft, uncomment this and set its YYYY-MM-DD
-   prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20131225/getusermedia.html",
+   prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20140218/getusermedia.html",
 
    // if there a publicly available Editor's Draft, this is the link
    edDraftURI:           "http://dev.w3.org/2011/webrtc/editor/getusermedia.html",
