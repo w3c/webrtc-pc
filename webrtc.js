744614a18e192a9756609a1eb69470d5e1b77559
Adam Bergkvist <adam.bergkvist@ericsson.com>
2014-07-04 13:18:33 +0200

    Added archived version 20140704.

--- webrtc.js
+++ webrtc.js
@@ -21,7 +21,7 @@ var respecConfig = {
   // copyrightStart: "2005",
 
   // if there is a previously published draft, uncomment this and set its YYYY-MM-DD
-  prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20140410/webrtc.html",
+  prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20140617/webrtc.html",
 
   // if there a publicly available Editor's Draft, this is the link
   edDraftURI: "http://dev.w3.org/2011/webrtc/editor/webrtc.html",
