81c4c074529b7d258c2bf3c76f9cb6fd26dc688d
Dan Burnett <dburnett@voxeo.com>
2014-08-17 09:01:31 -0400

    created 20140817 archive for gUM

--- getusermedia.js
+++ getusermedia.js
@@ -19,7 +19,7 @@ var respecConfig = {
    // copyrightStart: "2005",
 
    // if there is a previously published draft, uncomment this and set its YYYY-MM-DD
-   prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20140619/getusermedia.html",
+   prevED: "http://dev.w3.org/2011/webrtc/editor/archives/20140704/getusermedia.html",
 
    // if there a publicly available Editor's Draft, this is the link
    edDraftURI:           "http://dev.w3.org/2011/webrtc/editor/getusermedia.html",
