119e2f1883137c42ea12af177842f965c31a8100
Philip Jägenstedt <philip@foolip.org>
2017-04-22 01:46:01 +0700

    Add a link to web-platform-tests to the top of the spec
    A few other specs that have something similar:
    https://fetch.spec.whatwg.org/
    https://w3c.github.io/IndexedDB/
    https://notifications.spec.whatwg.org/
    https://w3c.github.io/ServiceWorker/
    https://webaudio.github.io/web-audio-api/
    https://xhr.spec.whatwg.org/

--- webrtc.js
+++ webrtc.js
@@ -85,6 +85,7 @@ var respecConfig = {
   // Team Contact.
   wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/47318/status",
   issueBase: "https://github.com/w3c/webrtc-pc/issues/",
+  testSuiteURI: "https://github.com/w3c/web-platform-tests/tree/master/webrtc",
   otherLinks: [
     {
       key: "Participate",
