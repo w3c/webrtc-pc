873aaf3bb975317b9ec3dc28bca803d1efd1f208
Jan-Ivar Bruaroey <wingboy@gmail.com>
2022-10-12 20:14:10 -0400

    Add infra

--- webrtc.js
+++ webrtc.js
@@ -293,7 +293,7 @@ var respecConfig = {
     }
 
   ],
-  xref: ["dom", "hr-time", "webidl", "html", "mediacapture-streams", "fileapi", "webrtc-stats", "websockets", "xhr"],
+  xref: ["dom", "hr-time", "webidl", "html", "mediacapture-streams", "fileapi", "webrtc-stats", "websockets", "xhr", "infra"],
   preProcess: [
     highlightTests,
     markTestableAssertions,
